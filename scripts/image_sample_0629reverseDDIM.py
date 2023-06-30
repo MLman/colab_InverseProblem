4a3064e0b8359a6d4a8cebad930b50fa69b39fb5
sojinleeme <sojinleeme@gmail.com>
2023-06-30 23:22:26 +0900

    Edit toy code clean

--- scripts/image_sample_0629reverseDDIM.py
+++ scripts/image_sample_0629reverseDDIM.py
@@ -5,6 +5,7 @@ numpy array. This can be used to produce samples for FID evaluation.
 
 import argparse
 import os
+import random
 
 import numpy as np
 import torch as th
@@ -14,25 +15,16 @@ from cm import dist_util, logger
 from cm.image_datasets_pairs import load_data_pairs
 import torchvision.utils as vtils
 
-# AFHQ
 from guided_diffusion.script_util import(
     NUM_CLASSES,
-    model_and_diffusion_defaults,
+    model_and_diffusion_defaults, # AFHQ
+    ffhq_model_and_diffusion_defaults, # FFHQ
+    imagenet_model_and_diffusion_defaults, # ImageNet
+    gopro_model_and_diffusion_defaults, # GoPro
     create_model_and_diffusion,
     add_dict_to_argparser,
     args_to_dict,
 )
-
-# FFHQ
-# from guided_diffusion.script_util_ffhq import(
-#     NUM_CLASSES,
-#     model_and_diffusion_defaults,
-#     create_model_and_diffusion,
-#     add_dict_to_argparser,
-#     args_to_dict,
-# )
-
-import random
 
 def set_random_seed(seed):
     random.seed(seed)
@@ -40,18 +32,12 @@ def set_random_seed(seed):
     th.manual_seed(seed)
     th.cuda.manual_seed_all(seed)
 
-
 def mkdir(path):
     if not os.path.exists(path):
         os.makedirs(path)
         
 def main():
     args = create_argparser().parse_args()
-    
-    if args.use_wandb:
-        import wandb
-        table_name = f"steps_{args.steps}_batch_{args.batch_size}"
-        wandb.init(project="toy", name=table_name)
     
     # set save directory
     args.log_suffix = f'{args.toy_exp}_{args.log_suffix}'
@@ -61,12 +47,36 @@ def main():
     mkdir(save_dir)
     
     dist_util.setup_dist(args.gpu)
-    # logger.configure(dir=save_dir)
-    logger.configure(dir=args.log_dir, format_strs=['stdout','log','csv','tensorboard'], log_suffix=args.log_suffix)
+    logger.configure(dir=save_dir, format_strs=['stdout','log','csv','tensorboard'], log_suffix=args.log_suffix)
 
     logger.log("creating model and diffusion...")
+    if 'afhq' in args.model_path:
+        image_dict = args_to_dict(args, model_and_diffusion_defaults().keys())
+        image_dict.update(model_and_diffusion_defaults())
+        args.wandb_table = 'Model:AFHQ'
+    elif 'ffhq' in args.model_path:
+        image_dict = args_to_dict(args, ffhq_model_and_diffusion_defaults().keys())
+        image_dict.update(ffhq_model_and_diffusion_defaults())
+        args.wandb_table = 'Model:FFHQ'
+    elif 'imagenet' in args.model_path:
+        image_dict = args_to_dict(args, imagenet_model_and_diffusion_defaults().keys())
+        image_dict.update(imagenet_model_and_diffusion_defaults())
+        args.wandb_table = 'Model:ImageNet'
+    elif 'GoPro' in args.model_path:
+        image_dict = args_to_dict(args, gopro_model_and_diffusion_defaults().keys())
+        image_dict.update(gopro_model_and_diffusion_defaults())
+        args.wandb_table = 'Model:GoPro'
+    else:
+        NotImplementedError
+        
+    if args.use_wandb:
+        import wandb
+        table_name = f"{args.wandb_table}_steps{args.steps}"
+        wandb.init(project="toy", name=table_name)
+        wandb.config.update(args)
+    
     model, diffusion = create_model_and_diffusion(
-        **args_to_dict(args, model_and_diffusion_defaults().keys()),
+        **image_dict,
     )
 
     model.load_state_dict(
@@ -99,7 +109,6 @@ def main():
 
         forward_dir = f'{save_dir}/{i}_forward_'
         backward_dir = f'{save_dir}/{i}_backward_'
-
 
         vtils.save_image(x_sharp, f'{save_dir}/Ori_sharp{i}.png', range=(-1,1), normalize=True)
         vtils.save_image(x_blur, f'{save_dir}/Ori_blur{i}.png', range=(-1,1), normalize=True)
@@ -162,6 +171,8 @@ def create_argparser():
         # steps=40,
         steps=1000,
 
+        # model_path='./models/imagenet/256x256_diffusion_uncond.pt',
+
         # data_dir="/hub_data1/sojin/afhq_blur/afhq_cat_motionblur",
         # model_path="./models/afhqCat/guided_diffusion_afhqcat_ema_0.9999_625000.pt",
         # seed=42,
@@ -169,11 +180,11 @@ def create_argparser():
 
         # data_dir="/hub_data1/sojin/ffhq_1K_motionblur",
         # model_path="./models/ffhq_1k/ffhq_10m.pt",
-        # seed=42,
+        seed=42,
         # seed=1234,
 
-        data_dir="/hub_data1/gopro"
-        model_path="./models/GoPro/clean/ema_0.9999_187500.pt",
+        # data_dir="/hub_data1/sojin/Restormer_GoPro/train",
+        # model_path="./models/GoPro/clean/ema_0.9999_187500.pt",
 
     )
     defaults.update(model_and_diffusion_defaults())
@@ -181,13 +192,14 @@ def create_argparser():
     
     parser.add_argument('--gpu', type=str, default='0')
     # parser.add_argument('--log_dir', type=str, default='/hub_data1/sojin/sampling_results/')
-    parser.add_argument('--log_dir', type=str, default='./toy230629/afhqcat_')
-    # parser.add_argument('--log_dir', type=str, default='./toy230629/gopro_')
-    # parser.add_argument('--log_dir', type=str, default='./toy230629/ffhq1K_')
+    parser.add_argument('--log_dir', type=str, default='./toy230629_ImageNet/afhqcat_')
+    # parser.add_argument('--log_dir', type=str, default='./toy230629/AFHQ_Cat')
+    # parser.add_argument('--log_dir', type=str, default='./toy230629/GoPro')
+    # parser.add_argument('--log_dir', type=str, default='./toy230629/FFHQ_1K')
     parser.add_argument('--toy_exp', type=str, default='ddim_reverse')
     # parser.add_argument('--toy_exp', type=str, default='None')
     parser.add_argument('-log','--log_suffix', type=str, required=True) # Experiment name, starts with tb(tesorboard) ->  tb_exp1
-    parser.add_argument('--use_wandb', type=bool, default=False) # Experiment name, starts with tb(tesorboard) ->  tb_exp1
+    parser.add_argument('--use_wandb', type=bool, default=True) # Experiment name, starts with tb(tesorboard) ->  tb_exp1
 
     add_dict_to_argparser(parser, defaults)
     return parser
